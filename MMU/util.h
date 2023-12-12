332190cf8610f494432711b626b69e6e238fd8b5
ChristianASC5 <cnicolas3411@gmail.com>
2023-12-12 01:16:30 -0500

    Lab 7 submission

---
+++ MMU/util.h
@@ -0,0 +1,13 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+//Collaborated with Lauren Guthrie and Ivan Stuart and used ChatGPT as a resource
+
+/**
+ * Utility function file
+ */
+
+
+void parse_file(FILE *, int [][2], int *, int *);
+
+#endif				// UTIL_H
